8bf47605e69b1eab48b7a707f580d37e5ab427f1
lp177 <luperez@student.42.fr>
2015-07-26 07:32:56 +0200

    Update .start.sh

--- .start.sh
+++ .start.sh
@@ -1,6 +1,6 @@
 MOUNT="/Volumes/myMountName"
 HOLD_HOME="/nfs/zfs-student-*/users/20[0-9][0-9]/`whoami`"
-HOLD_HOME=`eval echo ${TRY_PATH}`
+HOLD_HOME=`eval echo ${HOLD_HOME}`
 
 pkill -u `whoami`
 echo -n "source $MOUNT/.z42.sh" > $HOLD_HOME/.zshrc
