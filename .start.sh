57415708afea8c63167d4ec3d183ab5b742f6216
lp177 <luperez@student.42.fr>
2015-08-09 04:24:39 +0200

    Update .start.sh

--- .start.sh
+++ .start.sh
@@ -3,6 +3,10 @@ HOLD_HOME="/nfs/zfs-student-*/users/20[0-9][0-9]/`whoami`"
 HOLD_HOME=`eval echo ${HOLD_HOME}`
 
 pkill -u `whoami`
+
 echo -n "source $MOUNT/.z42.sh" > $HOLD_HOME/.zshrc
+
 source $MOUNT/.z42.sh
-cp -R $MOUNT/.brew $HOLD_HOME/.brew
+
+rm -rf $HOLD_HOME/.brew
+cp -pXRf $MOUNT/.brew $HOLD_HOME/.brew
