c975911e6c33393fe5b1a2be5175e7cdbbe7e279
lp177 <luperez@student.42.fr>
2015-08-09 05:19:35 +0200

    Update .start.sh

--- .start.sh
+++ .start.sh
@@ -1,12 +1,13 @@
 MOUNT="/Volumes/myMountName"
+
 HOLD_HOME="/nfs/zfs-student-*/users/20[0-9][0-9]/`whoami`"
 HOLD_HOME=`eval echo ${HOLD_HOME}`
 
+#import and execute distant zshrc
 pkill -u `whoami`
-
 echo -n "source $MOUNT/.z42.sh" > $HOLD_HOME/.zshrc
-
 source $MOUNT/.z42.sh
 
+#get .brew
 rm -rf $HOLD_HOME/.brew
 cp -pXRf $MOUNT/.brew $HOLD_HOME/.brew
