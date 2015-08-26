7332f656dc4c7ec7b3b27a9b426db709ac7bd3af
lp177 <luperez@student.42.fr>
2015-08-26 09:20:09 +0200

    Create .afpConnect.sh

---
+++ .afpConnect.sh
@@ -0,0 +1,38 @@
+AFP_HOME="/Volumes/afp177"
+
+MAC_HOME="/Volumes/Data/nfs/zfs-student-*/users/20[0-9][0-9]/`whoami`"
+MAC_HOME=`eval echo ${MAC_HOME}`
+
+export MAC_HOME
+export AFP_HOME
+
+PATH_BASE="/Volumes/afp177/app:/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin"
+
+ZSHRC="(.z42.sh|.zshrc)"
+
+alias goserv="
+	export HOME=$AFP_HOME;
+	cd
+"
+goserv
+alias gomac="
+	export HOME=$MAC_HOME;
+	cd
+"
+
+alias extractBrew="
+	rm -rf $AFP_HOME/._brew
+	mv $AFP_HOME/.brew $AFP_HOME/._brew
+	cp -pXRf $MAC_HOME/.brew $AFP_HOME/.brew
+"
+alias extractAtom="
+	rm -rf $AFP_HOME/._atom
+	mv $AFP_HOME/.atom $AFP_HOME/._atom
+	cp -pXRf $MAC_HOME/.atom $AFP_HOME/.atom
+"
+alias extractAll="
+	extractAtom
+	extractBrew
+"
+
+alias infect="cat AFP_HOME/.start > $MAC_HOME/.zshrc"
